63c7269a4b36f6bf29ae637d1831af5361844ee5
Kier Kilian <kierkilian@gmail.com>
2021-01-23 13:05:06 +0100

    dark mode als default

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:digitales_register_app/digReg/login_page.dart';
+import 'package:digitales_register_app/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'digReg/myHomePage.dart';
+import 'package:provider/provider.dart';
 void main() {
   runApp(MyApp());
 }
@@ -7,12 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider<ThemeChanger>(
+      create: (_) => ThemeChanger(ThemeData.dark()),
+      child: new MaterialAppWithTheme(),
+    );
+  }
+}
+
+class MaterialAppWithTheme extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeChanger>(context);
+
     return MaterialApp(
-      title: 'Digitales Register App (obo bessa)',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
+      home: LoginPage(),
+      theme: theme.getTheme(),
     );
   }
 }

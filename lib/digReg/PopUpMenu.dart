4b213b688eaf069aec26684995350a2382b69fb1
Kier Kilian <kierkilian@gmail.com>
2021-02-06 20:48:10 +0100

    Add Logout

--- lib/digReg/PopUpMenu.dart
+++ lib/digReg/PopUpMenu.dart
@@ -1,9 +1,11 @@
 class Constants{
   static const String Setting = "Settings";
   static const String Exit = "Exit";
+  static const String Logout = "Abmelden";
 
   static const List<String> choices = <String>[
     Setting,
     Exit,
+    Logout
   ];
 }
